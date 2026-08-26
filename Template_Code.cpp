e0e875c5cb4f19342e05ecba4e62d7d70489ed29
s-salmaezzat2007 <s-salma.ezzat@zewailcity.edu.eg>
2026-08-26 18:00:57 +0300

    my task is done:Implement the EmergencyCase class storing patientId and severity with operator< overloaded for max-heap ordering.
    Implement addEmergency(patientId) and handleEmergency() using std::queue<int>.
    
    Implement addPriorityEmergency(patientId, severity) validating severity range (1 to 5).
    
    Implement handlePriorityEmergency() using std::priority_queue<EmergencyCase> to dequeue and return the highest severity case first.

--- Template_Code.cpp
+++ Template_Code.cpp
@@ -30,13 +30,20 @@ private:
     int severity;
 
 public:
-    EmergencyCase(int pid, int s);
+    EmergencyCase(int pid, int s)
+    {
+        patientId = pid;
+        severity = s;
+    }
 
-    int getPatientId() const;
-    int getSeverity() const;
+
+    int getPatientId() const { return patientId; }
+    int getSeverity() const { return severity; }
 
     // Higher severity = higher priority
-    bool operator<(const EmergencyCase& other) const;
+    bool operator<(const EmergencyCase& other) const {
+        return severity < other.severity;
+    }
 };
 
 
@@ -166,7 +173,7 @@ private:
 
 
 public:
-
+   
     // Constructor
     Hospital();
 
@@ -191,11 +198,18 @@ public:
         RoomType type
     );
 
-    void addEmergency(
-        int patientId
-    );
+    void addEmergency(int patientId)
+    {
+        emergencyQueue.push(patientId);
+    }
 
-    int handleEmergency();
+    int handleEmergency()
+    {
+        if (emergencyQueue.empty()) return -1;
+        int patientId = emergencyQueue.front();
+        emergencyQueue.pop();
+        return patientId;
+    }
 
     void bookAppointment(
         int doctorId,
@@ -318,10 +332,15 @@ public:
     // Priority Emergency
     // ===================================================== //
 
-    void addPriorityEmergency(
-        int patientId,
-        int severity
-    );
+    void addPriorityEmergency(int patientId, int severity)
+    {
+        if (severity < 1 || severity > 5)
+        {
+        cout << "Invalid severity.Must be between 1 and 5." << endl;
+        return;
+        }
+        priorityEmergencyQueue.push(EmergencyCase(patientId, severity));
+    }
 
 
     // =====================================================
@@ -329,7 +348,13 @@ public:
     // Handle Priority Emergency
     // ===================================================== //
 
-    int handlePriorityEmergency();
+    int handlePriorityEmergency()
+    {
+        if (priorityEmergencyQueue.empty()) return -1;
+        EmergencyCase top = priorityEmergencyQueue.top();
+        priorityEmergencyQueue.pop();
+        return top.getPatientId();
+    }
 
 
     // =====================================================
